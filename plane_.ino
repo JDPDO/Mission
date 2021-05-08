55fcf79842320a9f212798f5f6a33b5f3d2599c3
Jan Petersen <11973633+JDPDO@users.noreply.github.com>
2021-05-08 20:00:20 +0200

    Add positions. Begin with controller setup.

--- plane_.ino
+++ plane_.ino
@@ -1,7 +1,11 @@
+#include<limits.h>
 #include<Servo.h>
+#include <stdio.h>
+
+// ### Basic Structures ###
 
 typedef struct Vector {
-  
+  float x, y, z;
 } VECTOR;
 
 typedef struct Controller {
@@ -18,39 +22,72 @@ typedef struct {
     int min, max; // for 0 to 180 deg; -1 for
   } PULSE;
   bool isRotor; // rotating servo
-  VECTOR forceDirection; // direction of force relative to plane
+  // Direction of force relative to plane.
+  // For non rotating servos direction is middle position  
+  VECTOR forceDirection;
+  // Position of force to custom zero point
+  VECTOR forcePosition;
 } SERVO;
 
+
+// ### Plane attributes ###
+
+// Local controllers
 const CONTROLLER CONTROLLERS[] = {
-  {{ 7, 6, 13}},
+  {{7, 6, 13}},
 };
 
+// Available servos
 const SERVO SERVOS[] = {
-  {{10}, {0, 20}, true, }, // Propeller
-  {{11}, {0, 20}, true}, // Rotor left
-  {{12}, {0, 20}, true}, // Rotor right
-  {{8}, {-1, -1}, false},
-  {{9}, {-1, -1}, false},
+  // {{pin}, {min, max}, isRotor, {forceDirection}, {forcePosition}
+  {{10}, {0, 20}, true, {0, 1, 0}, {0, 0, 0}}, // Propeller
+  {{11}, {0, 20}, true, {0, 0, 1}, {7, -8, 0}}, // Rotor left
+  {{12}, {0, 20}, true, {0, 0, 1}, {-7, -8, 0}}, // Rotor right
+  {{8}, {-1, -1}, false, {INT_MIN, 1, 0}, {0, -20, 2}}, // Elevator
+  {{9}, {-1, -1}, false, {0, 1, INT_MIN}, {0, -20, 3}}, // Rudder
 };
 
-Servo servos[3];
+VECTOR barycenter = {0, -10, 1}; // Position of center of mass.
+
+Servo servos[5]; // With all servos of plane!
+
+
+// ### Init and Setup plane ###
+
+VECTOR destination; // Calculated destination vector 
 
 void setup() {
   Serial.begin(9600);
   
   // Setup rotors
-  SERVO ro;  
+  SERVO ro;
+  // `SERVOS` and `servos` need to have same length.
   for(int i = 0; i < sizeof(servos); i++) {
     ro = SERVOS[i];
     pinMode(ro.PIN.pulse, INPUT);
-    servos[i].attach(ro.PIN.pulse, ro.PULSE.min, ro.PULSE.max);
+    servos[i].attach(
+      ro.PIN.pulse,
+      ro.PULSE.min == -1 ? NULL : ro.PULSE.min,
+      ro.PULSE.max == -1 ? NULL : ro.PULSE.max
+    );
   }
 
-  // Setup controllers
-  
+  // Setup local controllers
+  int controllers = sizeof(CONTROLLERS);
+  switch(controllers) {
+    case 0:
+      Serial.println("No local controller configured.");
+      break;
+    default:
+      Serial.println(controllers + " local controllers.");
+      Serial.println("So many controllers are not supported yet. Only set up two.");
+    case 2:
+      Serial.println("Using two local controllers.");
+      
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
